ac1623457b1d6d1181ca25462b1e58864cd90fbb
Kunaljadhav1606 <kunaljadhav1606@gmail.com>
2022-09-07 16:45:44 +0100

    UMA-10 :Provide Implementation for Service Interface methods

--- src/main/java/com/BikkadIT/UserManagementApplication/Repository/StateRepository.java
+++ src/main/java/com/BikkadIT/UserManagementApplication/Repository/StateRepository.java
@@ -1,6 +1,7 @@
 package com.BikkadIT.UserManagementApplication.Repository;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import com.BikkadIT.UserManagementApplication.entites.StateMasterEntity;
 @Repository
 public interface StateRepository extends JpaRepository<StateMasterEntity, Serializable>{
 
+	public List< StateMasterEntity> finByCountryId(Integer countryId);
 }
--- src/main/java/com/BikkadIT/UserManagementApplication/Service/UserServiceImpl.java
+++ src/main/java/com/BikkadIT/UserManagementApplication/Service/UserServiceImpl.java
@@ -1,10 +1,20 @@
 package com.BikkadIT.UserManagementApplication.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.BikkadIT.UserManagementApplication.Binding.LoginForm;
+import com.BikkadIT.UserManagementApplication.Repository.CityRepository;
+import com.BikkadIT.UserManagementApplication.Repository.CountryRepository;
+import com.BikkadIT.UserManagementApplication.Repository.StateRepository;
 import com.BikkadIT.UserManagementApplication.Repository.UserAccountRepository;
+import com.BikkadIT.UserManagementApplication.entites.CityMasterEntity;
+import com.BikkadIT.UserManagementApplication.entites.CountryMasterEntity;
+import com.BikkadIT.UserManagementApplication.entites.StateMasterEntity;
 import com.BikkadIT.UserManagementApplication.entites.UserAccountEntity;
 
 @Service
@@ -12,6 +22,16 @@ public class UserServiceImpl implements UserServiceI {
 	
 	@Autowired
 	private UserAccountRepository userAccountRepository;
+	
+	@Autowired
+	private CountryRepository countryRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
+	
+	@Autowired
+	private CityRepository cityRepository;
+	
 
 	@Override
 	public String loginCheck(LoginForm loginForm) {
@@ -28,6 +48,37 @@ public class UserServiceImpl implements UserServiceI {
 			
 			return "Creditional Are invalid";
 		}
+	}
+
+	@Override
+	public Map<Integer, String> getCountries() {
+		List<CountryMasterEntity> findAll = countryRepository.findAll();
+		Map<Integer ,String> countryMap=new HashMap<Integer , String>();
+		for(CountryMasterEntity u:findAll) {
+		    countryMap.put(u.getCountryId(), u.getCountryName());
+		}
+		return countryMap;
+	}
+
+	@Override
+	public Map<Integer, String> getStates(Integer countryId) {
+	List<StateMasterEntity> finByCountryId = stateRepository.finByCountryId(countryId);
+	Map<Integer , String> stateMap=new HashMap<Integer , String>();
+	for(StateMasterEntity u : finByCountryId) {
+		stateMap.put(u.getStateId(), u.getStateName());
+	}
+		
+		return stateMap;
+	}
+
+	@Override
+	public Map<Integer, String> getCities(Integer stateId) {
+		List<CityMasterEntity> findByStateId = cityRepository.findByStateId(stateId);
+		Map<Integer , String> cityMap=new HashMap<Integer , String >();
+		for(CityMasterEntity u : findByStateId) {
+			cityMap.put(u.getCityId(), u.getCityName());
+		}
+		return cityMap;
 	}
 	
 	
